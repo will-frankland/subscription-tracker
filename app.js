999304f95cdaa16a81fe8bc9ebc8ec5e7b8078cc
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2025-02-13 17:37:27 -0500

    setup env files and import dynamically

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 
+import { PORT } from "./config/env.js";
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send("Welcome to the subscription tracker API")
 });
 
-app.listen(3000, () => {
-  console.log('Subscription Tracker API is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`Subscription Tracker API is running on http://localhost:${PORT}`)
 })
 
 export default app;
