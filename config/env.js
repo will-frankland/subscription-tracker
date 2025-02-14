4a3624631c3253e8134b2e8ad029c55bb37f4f19
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2025-02-14 15:14:59 -0500

    create initial controllers and add into routes for Auth

--- config/env.js
+++ config/env.js
@@ -2,4 +2,10 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const { PORT, NODE_ENV, DB_URI } = process.env
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN
+} = process.env
---
+++ controllers/auth.controller.js
@@ -0,0 +1,7 @@
+export const signUp = async (req, res, next) => {}
+
+
+export const signIn = async (req, res, next) => {}
+
+
+export const signOut = async (req, res, next) => {}
