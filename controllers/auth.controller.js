3b66d4db6bb827a9a431ffbbacdb30b470a313f5
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2025-02-14 15:52:31 -0500

    create signup controller and test with postman

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,55 @@
-export const signUp = async (req, res, next) => {}
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 
+export const signUp = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
-export const signIn = async (req, res, next) => {}
+  try {
+    // Create new user
+    const { name, email, password } = req.body;
 
+    // Check if they exist?
+    const existingUser = await User.findOne({ email });
 
-export const signOut = async (req, res, next) => {}
+    if (existingUser) {
+      const error = new Error("User already exists");
+      error.statusCode = 409;
+      throw error;
+    }
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUsers = await User.create(
+      [{ name, email, password: hashedPassword }],
+      { session }
+    );
+
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        token,
+        user: newUsers[0],
+      },
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {};
+
+export const signOut = async (req, res, next) => {};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signIn, signOut, signUp } from "../controllers/auth.controller";
+import { signIn, signOut, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
+// Path example: /api/v1/auth/signUp (POST)
 authRouter.post("/sign-up", signUp);
 authRouter.post("/sign-in", signIn)
 authRouter.post("/sign-out", signOut)
