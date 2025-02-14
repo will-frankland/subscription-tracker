3551f174c94c64bbcf76b837c499d8d76b20490f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2025-02-14 10:44:49 -0500

    setup mongoDB connection

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.get('/', (req, res) => {
-  res.send("Welcome to the subscription tracker API")
+app.get("/", (req, res) => {
+  res.send("Welcome to the subscription tracker API");
 });
 
-app.listen(PORT, () => {
-  console.log(`Subscription Tracker API is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, async () => {
+  console.log(
+    `Subscription Tracker API is running on http://localhost:${PORT}`
+  );
+  await connectToDatabase();
+});
 
 export default app;
