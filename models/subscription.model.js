0d427823a372dbf417cb339ab4823cc7ef1982fc
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2025-02-14 10:52:43 -0500

    create User model

---
+++ models/subscription.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Username required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Please use a valid email email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
