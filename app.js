536dd2e4f4bc1f871a0a8e81a340a342c4a5fdde
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2025-02-14 14:57:33 -0500

    create error middleware and add json, urlencoded and cookieParser middlewares to app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { PORT } from "./config/env.js";
 
@@ -6,12 +7,19 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the subscription tracker API");
