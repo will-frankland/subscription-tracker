d6f9e2566c950ca59c78e9ec557a6cc632f3ccbc
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2025-02-15 15:16:15 -0500

    fix typo

--- middlewares/auth.middlware.js
+++ middlewares/auth.middlware.js
@@ -8,12 +8,12 @@ const authorize = async (req, res, next) => {
 
     if (
       req.headers.authorization &&
-      req.headers.authorization.staertsWith("Bearer")
+      req.headers.authorization.startsWith("Bearer")
     ) {
       token = req.headers.authorization.split(" ")[1];
     }
 
-    if (!token) return res.status(401).json({ message: "Authorized" });
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     const decoded = jwt.verify(token, JWT_SECRET);
 
