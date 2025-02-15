90accbb9ab35a7ac147acf4f3062122ea7c37f93
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2025-02-15 15:10:05 -0500

    create get User/s and auth middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
+import { getUser, getUsers } from '../controllers/user.controller.js'
+import authorize from '../middlewares/auth.middlware.js';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {
-  res.send({ title: 'GET all users' })
-})
-userRouter.get('/:id', (req, res) => {
-  res.send({ title: 'GET user details' })
-})
+userRouter.get('/', getUsers)
+
+userRouter.get('/:id', authorize, getUser)
+
 userRouter.post('/', (req, res) => {
   res.send({ title: 'CREATE new users' })
 })
